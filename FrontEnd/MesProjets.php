595bf9130a1779e3afc7949134041607e7dedc7a
Ayoub GTC <ayoubsnini@gmail.com>
2023-11-24 10:07:45 +0100

    frontend

---
+++ FrontEnd/MesProjets.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-scroll  shadow-0 border-bottom border-dark">
+      <div class="container">
+        <img src="../Image/log.png" alt="logo" class="rounded-4"  style="width: 80px; height: 60px;">
+        <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
+          data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+          aria-label="Toggle navigation">
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto d-flex gap-5">
+            <li class="nav-item">
+              <a class="nav-link" href="DashboardM.php">Mes Equipes</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#!">Mes Projets</a>
+            </li>
+            
+            <button type="button" class="btn btn-outline-danger  ms-3">Log Out</button>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Navbar -->
+    <h1 class="d-flex justify-content-center mt-5"> Mes Projets </h1>
+
+    <div class=" d-flex justify-content-center ">
+      <div class="col-md-10 px-2 ">
+        <table class="table mt-4">
+          <thead>
+             <tr class="height1">
+              <th class=" align-middle">ID_Equipe</th>
+              <th class=" align-middle">Date_creation</th>
+              <th class=" align-middle">Nom_equipe</th>
+              <th class=" align-middle">ID_Personnel</th>
+              <th class=" align-middle">Nom</th>
+              <th class=" align-middle">Prenom</th>
+              <th class=" align-middle">Email</th>
+              <th class=" align-middle">Telephone</th>
+              <th class=" align-middle">Role</th>
+              <th class=" align-middle">Statut</th>   
+             </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+
+    
+</body>
+</html>
---
+++ FrontEnd/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <section class="vh-100" style="background-color: #5483BC;">
+        <div class="container py-5 h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col col-xl-10">
+              <div class="card" style="border-radius: 1rem;">
+                <div class="row g-0">
+                  <div class="col-md-6 col-lg-5 d-none d-md-flex align-items-center">
+                    <img src="../Image/10705723_44658.jpg"
+                      alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+                  </div>
+                  <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                    <div class="card-body p-4 p-lg-5 text-black">
+      
+                      <form>
+      
+                        <div class="d-flex align-items-center justify-content-center mb-3 pb-1">
+                          <img src="../Image/log.png" alt="logo" style="width: 100px;">
+                        </div>
+      
+                        <h5 class="fw-semibold mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
+      
+                        <div class="form-floating mb-4">
+                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                            <label for="floatingInput" class="text-secondary">Email address</label>
+                          </div>
+                          <div class="form-floating mb-4 ">
+                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                            <label for="floatingPassword" class="text-secondary">Password</label>
+                          </div>
+      
+                        <div class="pt-1 mb-4 d-flex justify-content-end">
+                          <button class="btn btn-primary btn-lg btn-block" type="button">Login</button> 
+                        </div>
+      
+                        <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a href="Inscription.php"
+                            style="color: #393f81;">Register here</a></p>
+                        <a href="DashboardM.php" class="small text-muted">Terms of use.</a>
+                        <a href="#!" class="small text-muted">Privacy policy</a>
+                      </form>
+      
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    
+</body>
+</html>
